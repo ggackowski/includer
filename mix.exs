c04a2aaa4613f6fcbbd65af11ce0e4e61bd7ed9d
Grzegorz Gackowski <greggupl1@gmail.com>
2020-06-04 15:52:58 +0200

    minor updates

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Includer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      description: "Program that can include missing header files to the C source code.",
+      name: "Includer",
+      source_url: "http://github.com/ggackowski/includer",
       deps: deps(),
+      package: package(),
       escript: [
         main_module: Includer.CLI,
         comment: "Includer",
@@ -30,4 +34,11 @@ defmodule Includer.MixProject do
       {:floki, "~> 0.26.0"}
     ]
   end
+
+  defp package, do:
+    [
+    name: "includer",
+    licenses: ["OPL-1.0"],
+    links: %{"GitHub" => "https://github.com/ggackowski/includer"}
+    ]
 end
